96880720c24753f5c99cf46effb20f20aa2ea319
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-06-07 00:25:29 -0300

    :sparkles: feat: telaHome

---
+++ src/pages/TelaHome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Header from "../Components/Header";
+import BottomTabBar from "../Components/BottomTabBar";
+import ListAbrigos from "../Components/ListAbrigos";
+
+export default function TelaHome() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <ListAbrigos/>
+      <BottomTabBar/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "center",
+    backgroundColor:"#FFF",
+    height:"100%"
+  },
+  text: {
+    fontSize: 24,
+    alignSelf: "flex-start",
+    padding: 10,
+    paddingLeft: 20,
+    color: "#00819E",
+    fontWeight: 600
+  }
+});
